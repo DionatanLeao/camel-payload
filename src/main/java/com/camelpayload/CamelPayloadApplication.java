91a06754193f909b37cabfc125771da6cbd122ed
DionatanLeao <dionatan.ti@outlook.com>
2022-05-14 11:09:14 -0300

    Project created

---
+++ src/main/java/com/camelpayload/CamelPayloadApplication.java
@@ -0,0 +1,13 @@
+package com.camelpayload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelPayloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelPayloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camelpayload/CamelPayloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camelpayload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelPayloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
