f85367c1cb4285439a9d6310a5f73bccc9f274b3
DionatanLeao <dionatan.ti@outlook.com>
2022-05-21 12:48:52 -0300

    Implementando gerador de caracteres UUID

--- src/main/java/com/camel/payload/service/impl/TokenServiceImpl.java
+++ src/main/java/com/camel/payload/service/impl/TokenServiceImpl.java
@@ -1,14 +1,16 @@
 package com.camel.payload.service.impl;
 
 import java.util.Base64;
+import java.util.UUID;
 
 import com.camel.payload.service.TokenService;
 
 public class TokenServiceImpl implements TokenService {
-	
+
 	@Override
 	public String gerarToken() {
-		String token = Base64.getEncoder().encodeToString("Gerando token SSO".getBytes());
+		UUID uuid = UUID.randomUUID();
+		String token = Base64.getEncoder().encodeToString(uuid.toString().getBytes());
 		return token;
 	}
 }
